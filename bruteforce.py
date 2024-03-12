6f4deb620a8e05f4bfecce86f9d601c521e1be15
JasonBats <46994866+JasonBats@users.noreply.github.com>
2024-03-12 10:38:42 +0100

    Added decorator, custom itertools.combinations(), and pep8 work

--- bruteforce.py
+++ bruteforce.py
@@ -1,9 +1,8 @@
 import os
-import json
+import csv
 from model import Action
 import itertools
-import time
-import psutil
+from decorator import perfs_decorator
 
 
 def create_actions_objects():
@@ -12,15 +11,19 @@ def create_actions_objects():
     :return: Actions objects.
     """
     database_path = os.path.join(os.path.dirname(__file__),
-                                 'datas', 'actions.json')
-    with open(database_path, 'r', encoding="utf-8") as file:
-        data = json.load(file)
+                                 'datas', 'actions.csv')  # actions.csv / memtest.csv / dataset1.csv / dataset2.csv
+    with open(database_path, 'r') as csv_file:
+        data = csv.reader(csv_file, delimiter=",")
 
-    actions = []
+        actions = []
 
-    for action in data:
-        action_object = Action(**action)
-        actions.append(action_object)
+        for action in data:
+            action_object = Action(name=action[0],
+                                   price=int(float(action[1])),
+                                   benefit=int(float(action[2])))
+            if action_object.price > 0:
+                actions.append(action_object)
+    csv_file.close()
     return actions
 
 
@@ -31,11 +34,32 @@ def get_all_combinations():
     """
     actions = create_actions_objects()
     combinations = []
-    for L in range(0, len(actions) + 1):
+    for L in range(len(actions)):
         for i in itertools.combinations(actions, L):
             combinations.append(i)
     print('Toutes les combinaisons possibles ont été stockées')
     return combinations
+
+
+def custom_itertools_combinations(iterable, r):  # Detailed itertools.combinations
+    # combinations('ABCD', 2) --> AB AC AD BC BD CD
+    # combinations(range(4), 3) --> 012 013 023 123
+    pool = tuple(iterable)
+    n = len(pool)
+    if r > n:
+        return
+    indices = list(range(r))
+    yield tuple(pool[i] for i in indices)
+    while True:
+        for i in reversed(range(r)):
+            if indices[i] != i + n - r:
+                break
+        else:
+            return
+        indices[i] += 1
+        for j in range(i + 1, r):
+            indices[j] = indices[j - 1] + 1
+        yield tuple(pool[i] for i in indices)
 
 
 def get_combinations_values(combinations, budget):
@@ -64,7 +88,13 @@ def get_combinations_values(combinations, budget):
     print(f'Profit : :{round(best_combination[2], 2)}')
 
 
-ping = time.perf_counter()
-get_combinations_values(get_all_combinations(), budget=500)
-pong = time.perf_counter()
-print(f'Temps écoulé : {pong - ping:0.2f} secondes')
+@perfs_decorator
+def run_program():
+    """
+    Run the program and watches time and hardware use.
+    :return: Print out the results of the program.
+    """
+    get_combinations_values(get_all_combinations(), budget=500)
+
+
+run_program()  # TODO : Flake-8 sur le projet
