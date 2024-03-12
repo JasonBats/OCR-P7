2ed2cb7ea9ba4e561473d0b959b370bae6a3fe23
JasonBats <46994866+JasonBats@users.noreply.github.com>
2024-03-12 10:38:52 +0100

    Added decorator

---
+++ decorator.py
@@ -0,0 +1,16 @@
+import time
+import tracemalloc
+
+
+def perfs_decorator(func):
+    def wrapper():
+        tracemalloc.start()  # Start memory tracking
+        ping = time.perf_counter()  # Start time tracking
+        func()
+        _, peak = tracemalloc.get_traced_memory()  # Store memory peak in variable.
+        pong = time.perf_counter()  # Stop time tracking
+        tracemalloc.stop()  # Stop memory tracking
+        print(f'Peak : {peak / 1048576:.2f} MB')
+        print(f'Temps écoulé : {pong - ping:0.2f} secondes')
+
+    return wrapper
